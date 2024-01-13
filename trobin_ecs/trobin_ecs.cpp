1ca84fd6a9ce504d2a531066ed054181d4b60aa9
Battletrout <nathanmkemp@gmail.com>
2024-01-13 21:49:48 +0900

    Add project files.

---
+++ trobin_ecs/trobin_ecs.cpp
@@ -0,0 +1,12 @@
+﻿// trobin_ecs.cpp : Defines the entry point for the application.
+//
+
+#include "trobin_ecs.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
